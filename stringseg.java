fe16ad384ba3b528109fc1c48024133933d2d207
Selvakumar-A <sk7396030@gmail.com>
2025-03-19 15:25:56 +0530

    Add files via upload

---
+++ stringseg.java
@@ -0,0 +1,14 @@
+package session3;
+
+public class stringseg {
+	public static void main(String[] args) {
+		String s1 ="hello";
+		String s2 = new String("welcome");
+		char[] c= {'J','A','V','A'};
+		String s3= new String(c);
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+}
